64043daf9127afcd11b48fd79fe09df8be910776
GABRIEL FERNANDES LACERDA <gabriel_f_lacerda@estudante.sesisenai.org.br>
2024-09-09 17:27:54 -0300

    Atividade lista 03

---
+++ exercicios/src/components/Lista03/Atividade01_vendaValor.jsx
@@ -0,0 +1,20 @@
+// Um comerciante comprou um produto e quer vendê-lo com lucro de 
+// 45% se o valor da compra for menor que R$20,00; caso contrário, o lucro 
+// será de 30%. Faça um programa que entre com o valor do produto e mostre 
+// o valor de venda.
+
+
+
+
+
+import React from 'react'
+
+function Atividade01_vendaValor() {
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  )
+}
+
+export default Atividade01_vendaValor
